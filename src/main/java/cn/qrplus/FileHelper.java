37493e7c04bf76697b4599471a12409618dcba3d
loveqianqian <diaozhiwei2k@163.com>
2016-11-05 23:37:53 +0800

    init commit

--- src/main/java/cn/qrplus/FileHelper.java
+++ src/main/java/cn/qrplus/FileHelper.java
@@ -2,6 +2,9 @@ package cn.qrplus;
 
 import javax.swing.filechooser.FileSystemView;
 import java.io.*;
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * <p>ProjectName:manager</p>
@@ -12,23 +15,29 @@ import java.io.*;
  */
 public class FileHelper {
 
+    private static String myFileName = "";
+
+    public void getFileName(){
+        myFileName = "data" + getCurrentTime();
+    }
+
     public String getAdministratorPath() {
         FileSystemView fsv = FileSystemView.getFileSystemView();
         return String.valueOf(fsv.getHomeDirectory());
     }
 
-    public String getAbsPath(){
+    public String getAbsPath() {
         FileSystemView fsv = FileSystemView.getFileSystemView();
-        String administratorPath= String.valueOf(fsv.getHomeDirectory());
+        String administratorPath = String.valueOf(fsv.getHomeDirectory());
         int i = administratorPath.lastIndexOf("\\");
-        return administratorPath.substring(0,i);
+        return administratorPath.substring(0, i);
     }
 
     public void parseCode() throws IOException {
         String administratorPath = getAdministratorPath();
         String absPath = getAbsPath();
         File file = new File(administratorPath + "/data.txt");
-        File resultFile = new File(absPath+"/data/data.t");
+        File resultFile = new File("C:/Users/Administrator/data/" + myFileName);
         if (file.exists()) {
             if (resultFile.exists()) {
                 resultFile.delete();
@@ -37,6 +46,7 @@ public class FileHelper {
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
             BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile));
             String line;
+            reader.readLine();
             while ((line = reader.readLine()) != null) {
                 String temp = line.split("\\|\\|")[0];
                 writer.write(temp + "\n");
@@ -48,40 +58,77 @@ public class FileHelper {
     }
 
     public boolean solveManager() throws Exception {
-        File myFile = new File(getAbsPath()+"/data");
-        if (!myFile.exists()) {
-            myFile.mkdirs();
-        }
-        File file = new File(getAbsPath()+"/data/.manager.k");
-        String code = new CodeHelper().getCode();
-        if (!file.exists()) {
-            file.createNewFile();
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            writer.write(code + "\ntrue");
-            writer.flush();
-            writer.close();
+        String code = new CodeHelper().getMacAddress();
+        String secretPath = getAdministratorPath() + "/jre7/bin/amd64/config.properties";
+        String managePath=getAdministratorPath()+"/data/.manager.k";
+        if (isNotExistFile(secretPath)) {
+            saveConfig(code);
+            saveMangeK(true, code,managePath);
             return true;
-        } else {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            String msg = reader.readLine();
-            String result;
-            boolean flag;
-            if (!code.equals(msg)) {
-                result = msg + "\nfalse";
-                flag = false;
-            } else {
-                result = msg + "\ntrue";
-                flag = true;
+        }else{
+            BufferedReader reader1 = new BufferedReader(new InputStreamReader(new FileInputStream(new File(secretPath))));
+            String msg1 = reader1.readLine();
+            BufferedReader reader2 = new BufferedReader(new InputStreamReader(new FileInputStream(new File(managePath))));
+            String msg2 = reader2.readLine();
+            String msg3=string2MD5(code);
+            if (msg1.equals(msg2)&&msg1.equals(msg3)) {
+                saveMangeK(true,msg2,managePath);
+                return true;
+            }else{
+                saveMangeK(false,msg2,managePath);
+                return false;
             }
-            file.delete();
-            file.createNewFile();
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            writer.write(result);
-            writer.flush();
-            writer.close();
-            reader.close();
-            return flag;
         }
+    }
+
+    public boolean isExistFile(String fileName) {
+        File file = new File(fileName);
+        return file.exists();
+    }
+
+    public boolean isNotExistFile(String fileName) {
+        return !isExistFile(fileName);
+    }
+
+    public void saveMangeK(boolean flag, String mac,String path) throws Exception {
+        File file = new File(path);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(string2MD5(mac) + "\n" + flag + "\n" + myFileName);
+        writer.flush();
+        writer.close();
+    }
+
+    public void saveConfig(String mac) throws Exception {
+        String secretPath = getAdministratorPath() + "/jre7/bin/amd64/config.properties";
+        File file = new File(secretPath);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(string2MD5(mac) + getCurrentTime());
+        writer.flush();
+        writer.close();
+    }
+
+    public String string2MD5(String inStr) throws Exception {
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        char[] charArray = inStr.toCharArray();
+        byte[] byteArray = new byte[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++)
+            byteArray[i] = (byte) charArray[i];
+        byte[] md5Bytes = md5.digest(byteArray);
+        StringBuilder hexValue = new StringBuilder();
+        for (byte md5Byte : md5Bytes) {
+            int val = ((int) md5Byte) & 0xff;
+            if (val < 16)
+                hexValue.append("0");
+            hexValue.append(Integer.toHexString(val));
+        }
+        return hexValue.toString();
+    }
+
+    public String getCurrentTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        Date date = new Date();
+        return sdf.format(date);
     }
 
 
--- src/main/java/cn/qrplus/ManagerHome.java
+++ src/main/java/cn/qrplus/ManagerHome.java
@@ -4,6 +4,7 @@ public class ManagerHome {
 
     public static void main(String[] args) throws Exception {
         FileHelper fileHelper=new FileHelper();
+        fileHelper.getFileName();
         boolean flag = fileHelper.solveManager();
         if (flag) {
             fileHelper.parseCode();
