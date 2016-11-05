5441ea54b33e9ceb23c6e2aa3de9a34ba93ba923
loveqianqian <diaozhiwei2k@163.com>
2016-11-05 10:46:10 +0800

    init commit

---
+++ src/test/java/cn/qrplus/CodeHelperTest.java
@@ -0,0 +1,19 @@
+package cn.qrplus;
+
+import org.junit.Test;
+
+/**
+ * <p>ProjectName:manager</p>
+ * <p>Description:</p>
+ *
+ * @author:diaozhiwei
+ * @data:2016/11/3
+ */
+public class CodeHelperTest {
+
+    @Test
+    public void testGetCode() throws Exception {
+        String code = new CodeHelper().getCode();
+        System.out.println(code);
+    }
+}
